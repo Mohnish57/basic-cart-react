a8c04666b608245ec1c298335ef951b038faaab8
monu <sawlanimohnish@gmail.com>
2023-09-21 04:11:13 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,27 @@
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: "https://parakkatjewels.com/cdn/shop/files/enamel-bangle-peb10-24-004-parakkat-jewels-1_e8f3d938-7d75-4693-88cf-ad789104d58c.jpg?v=1691204329",
+      title: "ENAMEL BANGLE PEBA012",
+      desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product...",
+      price: "2299",
+    },
+    {
+      id: 2,
+      img: "https://staticimg.titan.co.in/Mia/Catalog/503822VWI1E00_1.jpg",
+      title: "BRACELET BANGLE",
+      desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product ...",
+      price: "2420",
+    },
+    {
+      id: 3,
+      img: "https://staticimg.titan.co.in/Mia/Catalog/503822VWM1E00_1.jpg",
+      title: "Golden Bangle",
+      desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product... ",
+      price: "1694",
+    },
+  ],
+};
+
+export default data;
